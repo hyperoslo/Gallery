b079d44b44e254512a3828a08e9f040905a0b964
Khoa Pham <onmyway133@gmail.com>
2016-08-25 15:23:28 +0200

    Update ShutterButton

--- Sources/Camera/ShutterButton.swift
+++ Sources/Camera/ShutterButton.swift
@@ -1,6 +1,64 @@
 import UIKit
+import Cartography
 
 class ShutterButton: UIButton {
 
-  
+  lazy var numberLabel: UILabel = self.makeNumberLabel()
+  lazy var roundLayer: CAShapeLayer = self.makeRoundLayer()
+
+  // MARK: - Initialization
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+
+    setup()
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: - Layout
+
+  override func layoutSubviews() {
+    super.layoutSubviews()
+
+    roundLayer.path = UIBezierPath(ovalInRect: CGRectInset(bounds, 3, 3)).CGPath
+    layer.cornerRadius = bounds.size.width/2
+  }
+
+  // MARK: - Setup
+
+  func setup() {
+    backgroundColor = UIColor.whiteColor()
+
+    layer.addSublayer(roundLayer)
+
+    addSubview(numberLabel)
+    numberLabel.translatesAutoresizingMaskIntoConstraints = false
+
+    constrain(numberLabel) { numberLabel in
+      numberLabel.center == numberLabel.superview!.center
+    }
+  }
+
+  // MARK: - Controls
+
+  func makeNumberLabel() -> UILabel {
+    let label = UILabel()
+    label.textColor = Config.Camera.ShutterButton.numberColor
+    label.font = UIFont.boldSystemFontOfSize(16)
+    label.text = "0"
+
+    return label
+  }
+
+  func makeRoundLayer() -> CAShapeLayer {
+    let layer = CAShapeLayer()
+    layer.strokeColor = Config.Camera.ShutterButton.numberColor.CGColor
+    layer.lineWidth = 2
+    layer.fillColor = nil
+
+    return layer
+  }
 }
--- Sources/Utils/Config.swift
+++ Sources/Utils/Config.swift
@@ -6,4 +6,11 @@ public struct Config {
     public static var backgroundColor: UIColor = UIColor(red: 0, green: 3/255, blue: 10/255, alpha: 1)
     public static var textColor: UIColor = UIColor.whiteColor()
   }
+
+  public struct Camera {
+
+    public struct ShutterButton {
+      public static var numberColor: UIColor = UIColor(red: 54/255, green: 56/255, blue: 62/255, alpha: 1)
+    }
+  }
 }
