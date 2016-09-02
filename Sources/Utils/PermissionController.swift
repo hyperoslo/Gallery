52f4b91a5fafa6b5865025f34d9903de01aebfd6
Khoa Pham <onmyway133@gmail.com>
2016-09-02 09:05:50 +0200

    Add PermissionController

---
+++ Sources/Utils/PermissionController.swift
@@ -0,0 +1,55 @@
+import UIKit
+import Cartography
+
+class PermissionController: UIViewController {
+
+  lazy var permissionView: PermissionView = self.makePermissionView()
+
+  // MARK: - Life cycle
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    setup()
+  }
+
+  // MARK: - Setup
+
+  func setup() {
+    view.addSubview(permissionView)
+    permissionView.translatesAutoresizingMaskIntoConstraints = false
+
+    permissionView.closeButton.addTarget(self, action: #selector(closeButtonTouched(_:)),
+                                         forControlEvents: .TouchUpInside)
+    permissionView.settingButton.addTarget(self, action: #selector(settingButtonTouched(_:)),
+                                           forControlEvents: .TouchUpOutside)
+
+    constrain(permissionView) {
+      permissionView in
+
+      permissionView.edges == permissionView.superview!.edges
+    }
+  }
+
+  // MARK: - Action
+
+  func settingButtonTouched(button: UIButton) {
+    Dispatch.main {
+      if let settingsURL = NSURL(string: UIApplicationOpenSettingsURLString) {
+        UIApplication.sharedApplication().openURL(settingsURL)
+      }
+    }
+  }
+
+  func closeButtonTouched(button: UIButton) {
+    EventHub.shared.close?()
+  }
+
+  // MARK: - Controls
+
+  func makePermissionView() -> PermissionView {
+    let view = PermissionView()
+
+    return view
+  }
+}
---
+++ Sources/Utils/PermissionView.swift
@@ -0,0 +1,73 @@
+import UIKit
+import Cartography
+
+class PermissionView: UIView {
+
+  lazy var label: UILabel = self.makeLabel()
+  lazy var settingButton: UIButton = self.makeSettingButton()
+  lazy var closeButton: UIButton = self.makeCloseButton()
+
+  // MARK: - Initialization
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+
+    setup()
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: - Setup
+
+  func setup() {
+    [label, settingButton, closeButton].forEach {
+      addSubview($0)
+      $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    constrain(closeButton, label, settingButton) {
+      closeButton, label, settingButton in
+
+      closeButton.top == closeButton.superview!.top
+      closeButton.left == closeButton.superview!.left
+      closeButton.width == 44
+      closeButton.height == 44
+
+      label.centerY == label.superview!.centerY
+      label.left == label.superview!.left + 50
+      label.right == label.superview!.right - 50
+
+      settingButton.top == label.bottom + 30
+      settingButton.centerX == settingButton.superview!.centerX
+    }
+  }
+
+  // MARK: - Controls
+
+  func makeLabel() -> UILabel {
+    let label = UILabel()
+    label.textColor = UIColor.blackColor()
+    label.font = Config.Font.Text.regular.fontWithSize(20)
+    label.text = "Please enable Photos and Camera"
+    label.textAlignment = .Center
+    label.numberOfLines = 0
+
+    return label
+  }
+
+  func makeSettingButton() -> UIButton {
+    let button = UIButton(type: .System)
+    button.setTitle("Go to Settings", forState: .Normal)
+
+    return button
+  }
+
+  func makeCloseButton() -> UIButton {
+    let button = UIButton(type: .Custom)
+    button.setImage(Bundle.image("gallery_close"), forState: .Normal)
+
+    return button
+  }
+}
