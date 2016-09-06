7ee8e3e2961ddd49602289927020cb00201b2e13
Khoa Pham <onmyway133@gmail.com>
2016-09-06 16:43:38 +0200

    Add AVAsset extension methods

--- Sources/Utils/Extensions/AVAsset+Extensions.swift
+++ Sources/Utils/Extensions/AVAsset+Extensions.swift
@@ -3,10 +3,23 @@ import AVFoundation
 extension AVAsset {
 
   var g_size: CGSize {
-    return .zero
+    return tracksWithMediaType(AVMediaTypeVideo).first?.naturalSize ?? .zero
+  }
+
+  var g_isPortrait: Bool {
+    return g_size.height > g_size.width
   }
 
   var g_fileSize: Double {
-    return 0
+    guard let avURLAsset = self as? AVURLAsset else { return 0 }
+
+    var result: AnyObject?
+    try? avURLAsset.URL.getResourceValue(&result, forKey: NSURLFileSizeKey)
+
+    if let result = result as? NSNumber {
+      return result.doubleValue
+    } else {
+      return 0
+    }
   }
 }
