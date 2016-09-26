5764b3e42ca30c3bfdb10d452c7389006e819b98
Khoa Pham <onmyway133@gmail.com>
2016-09-26 13:05:03 +0200

    Setup inputs and outputs

--- Sources/Utils/VideoEditor/AdvancedVideoEditor.swift
+++ Sources/Utils/VideoEditor/AdvancedVideoEditor.swift
@@ -5,9 +5,14 @@ import Photos
 public class AdvancedVideoEditor: VideoEditing {
 
   var writer: AVAssetWriter!
-  var writerInput: AVAssetWriterInput!
+  var videoInput: AVAssetWriterInput?
+  var audioInput: AVAssetWriterInput?
+
   var reader: AVAssetReader!
-  var readerOutput: AVAssetReaderOutput!
+  var videoOutput: AVAssetReaderVideoCompositionOutput?
+  var audioOutput: AVAssetReaderAudioMixOutput?
+
+  let queue = dispatch_queue_create("no.hyper.Gallery.AdvancedVideoEditor.Queue", dispatch_queue_attr_make_with_qos_class(DISPATCH_QUEUE_SERIAL, QOS_CLASS_BACKGROUND, 0))
 
   // MARK: - Initialization
 
@@ -23,8 +28,94 @@ public class AdvancedVideoEditor: VideoEditing {
       return
     }
 
-    let writer = try? AVAssetWriter(URL: outputURL, fileType: EditInfo.file.type)
-  
+    guard let writer = try? AVAssetWriter(URL: outputURL, fileType: EditInfo.file.type),
+      reader = try? AVAssetReader(asset: avAsset)
+    else {
+      completion(nil)
+      return
+    }
+
+    self.writer = writer
+    self.reader = reader
+
+    wire(avAsset)
+
+    writer.startWriting()
+    reader.startReading()
+    writer.startSessionAtSourceTime(kCMTimeZero)
+
+    // Video
+    if let videoOutput = videoOutput, videoInput = videoInput {
+      videoInput.requestMediaDataWhenReadyOnQueue(queue) {
+        self.stream(from: videoOutput, to: videoInput)
+      }
+    }
+
+    // Audio
+    if let audioOutput = audioOutput, audioInput = audioInput {
+      audioInput.requestMediaDataWhenReadyOnQueue(queue) {
+        self.stream(from: audioOutput, to: audioInput)
+      }
+    }
+
+    writer.finishWritingWithCompletionHandler {
+      completion(outputURL)
+    }
+  }
+
+  // MARK: - Helper
+
+  private func wire(avAsset: AVAsset) {
+    // Video
+    let videoTracks = avAsset.tracksWithMediaType(AVMediaTypeVideo)
+    if !videoTracks.isEmpty {
+      // Output
+      let videoOutput = AVAssetReaderVideoCompositionOutput(videoTracks: videoTracks, videoSettings: nil)
+      videoOutput.videoComposition = EditInfo.composition(avAsset)
+      if reader.canAddOutput(videoOutput) {
+        reader.canAddOutput(videoOutput)
+      }
+
+      // Input
+      let videoInput = AVAssetWriterInput(mediaType: AVMediaTypeVideo, outputSettings: nil)
+      if writer.canAddInput(videoInput) {
+        writer.addInput(videoInput)
+      }
+
+      self.videoInput = videoInput
+      self.videoOutput = videoOutput
+    }
+
+    // Audio
+    let audioTracks = avAsset.tracksWithMediaType(AVMediaTypeAudio)
+    if !audioTracks.isEmpty {
+      // Output
+      let audioOutput = AVAssetReaderAudioMixOutput(audioTracks: audioTracks, audioSettings: nil)
+      if reader.canAddOutput(audioOutput) {
+        reader.addOutput(audioOutput)
+      }
+
+      // Input
+      let audioInput = AVAssetWriterInput(mediaType: AVMediaTypeAudio, outputSettings: nil)
+      if writer.canAddInput(audioInput) {
+        writer.addInput(audioInput)
+      }
+
+      self.audioOutput = audioOutput
+      self.audioInput = audioInput
+    }
+  }
+
+  private func stream(from output: AVAssetReaderOutput, to input: AVAssetWriterInput) {
+    while input.readyForMoreMediaData {
+      guard let buffer = output.copyNextSampleBuffer()
+      else {
+        input.markAsFinished()
+        break
+      }
+
+      input.appendSampleBuffer(buffer)
+    }
   }
 }
 
