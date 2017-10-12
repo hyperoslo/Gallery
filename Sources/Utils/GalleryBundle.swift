97079d6608589e7f3bdba4ac401a31e40ffb353b
Khoa Pham <onmyway133@gmail.com>
2017-10-12 13:46:37 +0200

    Change to GalleryBundle.self

--- Sources/Utils/GalleryBundle.swift
+++ Sources/Utils/GalleryBundle.swift
@@ -3,7 +3,7 @@ import UIKit
 class GalleryBundle {
 
   static func image(_ named: String) -> UIImage? {
-    let bundle = Foundation.Bundle(for: Bundle.self)
+    let bundle = Foundation.Bundle(for: GalleryBundle.self)
     return UIImage(named: "Gallery.bundle/\(named)", in: bundle, compatibleWith: nil)
   }
 }
