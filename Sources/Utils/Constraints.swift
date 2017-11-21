87fd4a89710e946226e8a4383e15be6018bc3981
Khoa Pham <onmyway133@gmail.com>
2017-11-21 13:44:40 +0100

    Add Constraints

--- Sources/Utils/Constraints.swift
+++ Sources/Utils/Constraints.swift
@@ -74,3 +74,17 @@ extension UIView {
     g_pin(on: .centerY, view: view)
   }
 }
+
+// https://github.com/hyperoslo/Sugar/blob/master/Sources/iOS/Constraint.swift
+struct Constraint {
+  static func on(constraints: [NSLayoutConstraint]) {
+    constraints.forEach {
+      ($0.firstItem as? UIView)?.translatesAutoresizingMaskIntoConstraints = false
+      $0.isActive = true
+    }
+  }
+
+  static func on(_ constraints: NSLayoutConstraint ...) {
+    on(constraints: constraints)
+  }
+}
