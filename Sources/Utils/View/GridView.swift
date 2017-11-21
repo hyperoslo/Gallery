8eeaf25cde15b67ab8778c13c27f5a793108d743
Khoa Pham <onmyway133@gmail.com>
2017-11-21 13:57:05 +0100

    Fix layout in GridView for top bar

--- Sources/Utils/View/GridView.swift
+++ Sources/Utils/View/GridView.swift
@@ -43,8 +43,22 @@ class GridView: UIView {
       bottomView.addSubview($0 as! UIView)
     }
 
-    topView.g_pinUpward()
-    topView.g_pin(height: 40)
+    Constraint.on(
+      topView.leftAnchor.constraint(equalTo: topView.superview!.leftAnchor),
+      topView.rightAnchor.constraint(equalTo: topView.superview!.rightAnchor),
+      topView.heightAnchor.constraint(equalToConstant: 40)
+    )
+
+    if #available(iOS 11, *) {
+      Constraint.on(
+        topView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor)
+      )
+    } else {
+      Constraint.on(
+        topView.topAnchor.constraint(equalTo: topView.superview!.topAnchor)
+      )
+    }
+
     bottomView.g_pinDownward()
     bottomView.g_pin(height: 80)
 
