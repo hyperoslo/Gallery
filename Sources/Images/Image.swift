b377cc2e933894a4d381ec3e9e0bb06ffdbf38aa
Khoa Pham <onmyway133@gmail.com>
2017-11-24 14:23:58 +0100

    Add resolve images

--- Sources/Images/Image.swift
+++ Sources/Images/Image.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Photos
 
+/// Wrap a PHAsset
 public class Image: Equatable {
 
   public let asset: PHAsset
@@ -15,22 +16,47 @@ public class Image: Equatable {
 // MARK: - UIImage
 
 extension Image {
-  public func uiImage(ofSize size: CGSize) -> UIImage? {
+
+  /// Resolve UIImage synchronously
+  ///
+  /// - Parameter size: The target size
+  /// - Returns: The resolved UIImage, otherwise nil
+  public func resolve(size: CGSize) -> UIImage? {
     let options = PHImageRequestOptions()
     options.isSynchronous = true
+    options.isNetworkAccessAllowed = true
 
     var result: UIImage? = nil
 
-    PHImageManager.default().requestImage(for: asset, targetSize: size, contentMode: .aspectFill, options: options) { (image, _) in
-      result = image
+    PHImageManager.default().requestImage(
+      for: asset,
+      targetSize: size,
+      contentMode: .aspectFill,
+      options: options) { (image, _) in
+        result = image
     }
 
     return result
+  }
+
+  /// Resolve an array of Image
+  ///
+  /// - Parameters:
+  ///   - images: The array of Image
+  ///   - size: The target size for all images
+  ///   - completion: Called when operations completion
+  public static func resolve(images: [Image], size: CGSize, completion: @escaping ([UIImage?]) -> Void) {
+    DispatchQueue.global().async {
+      let uiImages = images.map({ $0.resolve(size: size) })
+      DispatchQueue.main.async {
+        completion(uiImages)
+      }
+    }
   }
 }
 
 // MARK: - Equatable
 
-public func ==(lhs: Image, rhs: Image) -> Bool {
+public func == (lhs: Image, rhs: Image) -> Bool {
   return lhs.asset == rhs.asset
 }
