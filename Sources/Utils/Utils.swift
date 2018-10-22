364488c38918156d2cb23b8c85d71b02930d602d
Christine Pühringer <christine.puehringer@apploft.de>
2018-10-22 11:22:04 +0200

    Fix for default portrait orientation when phone is face up or down: using the status bar orientation now to determine the orientation of the phone in case of face up or down

--- Sources/Utils/Utils.swift
+++ Sources/Utils/Utils.swift
@@ -28,7 +28,18 @@ struct Utils {
     case .portraitUpsideDown:
       return .portraitUpsideDown
     default:
-      return .portrait
+        switch UIApplication.shared.statusBarOrientation {
+        case .portrait:
+            return .portrait
+        case .portraitUpsideDown:
+            return .portraitUpsideDown
+        case .landscapeLeft:
+            return .landscapeLeft
+        case .landscapeRight:
+            return .landscapeRight
+        case .unknown:
+            return .portrait
+        }
     }
   }
 
