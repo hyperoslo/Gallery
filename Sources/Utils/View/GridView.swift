aa7b9f931d48ed915b5ff614d2a3f3715546c166
Vlada Radchenko <vlada.radchenko@nixsolutions.com>
2019-10-01 18:16:34 +0300

    update album color

--- Sources/Utils/View/GridView.swift
+++ Sources/Utils/View/GridView.swift
@@ -87,7 +87,7 @@ class GridView: UIView {
 
   private func makeTopView() -> UIView {
     let view = UIView()
-    view.backgroundColor = UIColor.white
+    view.backgroundColor = .backgroundColor
 
     return view
   }
